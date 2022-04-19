a1cbcd62dca06ce6e18732f8eedd9ad3eaacf027
heatherbaier <hmbaier@email.wm.edu>
2022-04-19 10:50:23 -0400

    nigeria coords & ids done; nepal data added

--- scripts/ids/generate_nig_ids.py
+++ scripts/ids/generate_nig_ids.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
 
-nig_ef = pd.read_csv("./data/nigeria/educational-facilities-in-nigeria.csv")
-nig_ef = nig_ef[["facility_name", "latitude", "longitude"]]
+nig_ef = pd.read_csv("../../data/NIG/educational-facilities-in-nigeria.csv")
+nig_ef = nig_ef[["facility_name"]]
 nig_ef = nig_ef.reset_index()
 nig_ef['geo_id'] = nig_ef['index'].apply(lambda x: 'NIG-{0:0>6}'.format(x))
-nig_ef = nig_ef.drop(["facility_name"], axis = 1)
-nig_ef = nig_ef[["geo_id", "longitude", "latitude"]]
+nig_ef["deped_id"] = None
+nig_ef = nig_ef[["geo_id", "deped_id", "facility_name"]]
+nig_ef = nig_ef.rename(columns = {"facility_name":"school_name"})
 
 print(nig_ef.head())
 
 
-nig_ef.to_csv("./files_for_db/coordinates/nig_coordinates.csv")
+nig_ef.to_csv("../../files_for_db/ids/nig_ids.csv", index = False)
