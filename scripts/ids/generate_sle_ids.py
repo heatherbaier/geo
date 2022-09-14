c68813694bb26dad0967cd95dd5c89e05491c3cc
heatherbaier <hmbaier@email.wm.edu>
2022-09-14 16:28:45 -0400

    done redoing ids #1

--- scripts/ids/generate_sle_ids.py
+++ scripts/ids/generate_sle_ids.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 
 sle_ef = pd.read_csv("../../data/SLE/sleeducptstewardschools.csv")
-sle_ef = sle_ef[["Educ_Code", "Educ_Name"]]
+print(sle_ef.columns)
+sle_ef = sle_ef[["Educ_Code", "Educ_Name", 'Adm2_Name', 'Adm3_Name', 'Adm4_Name']].rename(columns = {'Adm2_Name':"adm1", 'Adm3_Name': "adm2", 'Adm4_Name':"adm3"})
 sle_ef = sle_ef.reset_index()
 sle_ef['geo_id'] = sle_ef['index'].apply(lambda x: 'SLE-{0:0>6}'.format(x))
-sle_ef = sle_ef[["geo_id", "Educ_Code", "Educ_Name"]].rename(columns = {"Educ_Code": "deped_id", "Educ_Name": "school_name"})
+sle_ef["address"] = "None"
+sle_ef["adm0"] = "SLE"
+sle_ef = sle_ef[["geo_id", "Educ_Code", "Educ_Name", "address","adm0","adm1","adm2","adm3"]].rename(columns = {"Educ_Code": "deped_id", "Educ_Name": "school_name"})
 
 print(sle_ef.head())
 
