646aaab1bfb2ad40f6bd3c2e780e0dcc2baa5fce
heatherbaier <hmbaier@email.wm.edu>
2022-09-14 16:28:45 -0400

    done redoing ids #1

--- scripts/ids/generate_bol_ids.py
+++ scripts/ids/generate_bol_ids.py
@@ -1,13 +1,38 @@
 import geopandas as gpd
 import pandas as pd
 
+from utils import *
+
 
 bol_ef = gpd.read_file("../../data/BOL/shp/EstabEducativos/EstabEducativos.shp")
-bol_ef = bol_ef[["gml_id"]]
+bol_ef = bol_ef[["gml_id", "POINT_X", "POINT_Y"]].rename(columns = {"POINT_Y": "latitude", "POINT_X": "longitude"})
 bol_ef = bol_ef.reset_index()
 bol_ef['geo_id'] = bol_ef['index'].apply(lambda x: 'BOL-{0:0>6}'.format(x))
 bol_ef["school_name"] = None
-bol_ef = bol_ef[["geo_id", "gml_id", "school_name"]].rename(columns = {"gml_id": "deped_id"})
+bol_ef = bol_ef[["geo_id", "gml_id", "school_name", "latitude", "longitude"]].rename(columns = {"gml_id": "deped_id"})
+bol_ef["address"] = None
+bol_ef["adm0"] = "BOL"
+print(bol_ef.head())
+
+# Geocode to ADM levels
+cols = ["geo_id", "deped_id", "school_name", "longitude", "latitude", "address", "adm0"]
+for adm in range(1, 4):
+
+    try:
+
+        cols += ["adm" + str(adm)]
+        downloadGB("BOL", str(adm), "../../gb")
+        shp = gpd.read_file(getGBpath("BOL", str(adm), "../../gb"))
+        bol_ef = gpd.GeoDataFrame(bol_ef, geometry = gpd.points_from_xy(bol_ef.longitude, bol_ef.latitude))
+        bol_ef = gpd.tools.sjoin(bol_ef, shp, how = "left").rename(columns = {"shapeName": "adm" + str(adm)})[cols]
+        print(bol_ef)
+
+    except Exception as e:
+
+        bol_ef["adm" + str(adm)] = None
+        print(e)
+
+bol_ef = bol_ef[cols].drop(["longitude", "latitude"], axis = 1)
 
 print(bol_ef.head())
 
--- scripts/ids/generate_phl_ids.py
+++ scripts/ids/generate_phl_ids.py
@@ -1,16 +1,20 @@
+import geopandas as gpd
 import pandas as pd
+
+from utils import *
 
 
 phl_ef = pd.read_csv("../../data/PHL/this_one.csv")
-phl_ef = phl_ef[["school_id", "school_name"]]
+print(phl_ef.columns)
+phl_ef = phl_ef[["school_id", "school_name", "longitude", "latitude", "region", "division", "province"]]
 phl_ef = phl_ef.drop_duplicates(subset = ["school_id"])
-
 phl_ef = phl_ef.reset_index()
 phl_ef['geo_id'] = phl_ef['index'].apply(lambda x: 'PHL-{0:0>6}'.format(x))
-phl_ef = phl_ef[["geo_id", "school_id", "school_name"]].rename(columns = {"school_id": "deped_id"})
+phl_ef["adm0"] = "PHL"
+phl_ef["address"] = None
+phl_ef = phl_ef[["geo_id", "school_id", "school_name", "address", "adm0", "region", "division", "province"]].rename(columns = {"school_id": "deped_id", "region": "adm1", "division":"adm2", "province": "adm3"})
+phl_ef = phl_ef[["geo_id","deped_id","school_name","address","adm0","adm1","adm2","adm3"]]
 
 print(phl_ef.head())
 
 phl_ef.to_csv("../../files_for_db/ids/phl_ids.csv", index = False)
-
-
