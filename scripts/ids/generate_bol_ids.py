85ade18fc15c443737d386b09e81a9ddaa2cb0b0
heatherbaier <hmbaier@email.wm.edu>
2024-01-31 16:51:50 -0500

    geo

--- scripts/ids/generate_bol_ids.py
+++ scripts/ids/generate_bol_ids.py
@@ -14,43 +14,44 @@ bol_ef = bol_ef.drop_duplicates(subset = ["latitude", "longitude"])
 
 print(bol_ef.shape)
 
-
-
-bol_ef = bol_ef.reset_index()
-bol_ef['geo_id'] = bol_ef['index'].apply(lambda x: 'BOL-{0:0>6}'.format(x))
-bol_ef["school_name"] = None
-bol_ef = bol_ef[["geo_id", "gml_id", "school_name", "latitude", "longitude"]].rename(columns = {"gml_id": "deped_id"})
-bol_ef["address"] = None
-bol_ef["adm0"] = "BOL"
 print(bol_ef.head())
 
-# print(bol_ef["geo_id"].value_counts())
 
-# agad
+# bol_ef = bol_ef.reset_index()
+# bol_ef['geo_id'] = bol_ef['index'].apply(lambda x: 'BOL-{0:0>6}'.format(x))
+# bol_ef["school_name"] = None
+# bol_ef = bol_ef[["geo_id", "gml_id", "school_name", "latitude", "longitude"]].rename(columns = {"gml_id": "deped_id"})
+# bol_ef["address"] = None
+# bol_ef["adm0"] = "BOL"
+# print(bol_ef.head())
 
-# Geocode to ADM levels
-cols = ["geo_id", "deped_id", "school_name", "longitude", "latitude", "address", "adm0"]
-for adm in range(1, 4):
+# # print(bol_ef["geo_id"].value_counts())
 
-    try:
+# # agad
 
-        cols += ["adm" + str(adm)]
-        downloadGB("BOL", str(adm), "../../gb")
-        shp = gpd.read_file(getGBpath("BOL", str(adm), "../../gb"))
-        bol_ef = gpd.GeoDataFrame(bol_ef, geometry = gpd.points_from_xy(bol_ef.longitude, bol_ef.latitude))
-        bol_ef = gpd.tools.sjoin(bol_ef, shp, how = "left").rename(columns = {"shapeName": "adm" + str(adm)})[cols]
-        print(bol_ef)
+# # Geocode to ADM levels
+# cols = ["geo_id", "deped_id", "school_name", "longitude", "latitude", "address", "adm0"]
+# for adm in range(1, 4):
 
-    except Exception as e:
+#     try:
 
-        bol_ef["adm" + str(adm)] = None
-        print(e)
+#         cols += ["adm" + str(adm)]
+#         downloadGB("BOL", str(adm), "../../gb")
+#         shp = gpd.read_file(getGBpath("BOL", str(adm), "../../gb"))
+#         bol_ef = gpd.GeoDataFrame(bol_ef, geometry = gpd.points_from_xy(bol_ef.longitude, bol_ef.latitude))
+#         bol_ef = gpd.tools.sjoin(bol_ef, shp, how = "left").rename(columns = {"shapeName": "adm" + str(adm)})[cols]
+#         print(bol_ef)
 
-bol_ef = bol_ef[cols].drop(["longitude", "latitude"], axis = 1)
+#     except Exception as e:
 
-print(bol_ef.head())
+#         bol_ef["adm" + str(adm)] = None
+#         print(e)
 
-print(bol_ef.shape)
+# bol_ef = bol_ef[cols].drop(["longitude", "latitude"], axis = 1)
+
+# print(bol_ef.head())
+
+# print(bol_ef.shape)
 
 
 
